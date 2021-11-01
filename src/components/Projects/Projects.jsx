b8d9c8a1d968babefeec208323b4473d907b79fd
kalebmcc <kalebl33mccullough@gmail.com>
2021-11-01 06:08:46 -0500

    Changed favicon

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -13,9 +13,10 @@ export default function Projects() {
                 <div className="text-container">
                     <h2>Dinner Dash</h2>
                     <div className='button-holder'>
-                    <a className="website" href='https://dinnerdashproject.herokuapp.com/' target="_blank">VIEW</a>
+                    <a className="website" href='https://dinnerdash-project.netlify.app/' target="_blank">SITE</a>
+                    <a className="website" href='https://github.com/kalebmcc/DinnerDash' target="_blank">REPO</a>
                     </div>
-                    <p>Dinner Dash is a full-stack application that </p><br />
+                    <p>Dinner Dash is a full-stack application that is able to allow for multiple users to all share their favorite recipes with each other. Each user is able to edit and delete their own posts with the authorization token from Django. </p><br />
                     <h4>Technologies Used:</h4>
                         <p>Django, PostgreSQL, and React</p>
 
@@ -33,6 +34,7 @@ export default function Projects() {
                     <h2>ParkPass</h2>
                     <div className='button-holder'>
                     <a className="website" href='https://parkpassadventure.netlify.app/' target="_blank">VIEW</a>
+                    <a className="website" href='https://github.com/SEIR-809-Hackers/seir-project-3' target="_blank">REPO</a>
                     </div>
                     <p>ParkPass is a full-stack application that allows a user to search through a database of national parks and save them. When saved, a park will be added to a user’s page and displayed as a card. Users can then check off if they’ve hiked at the selected park.</p><br />
                     <h4>Technologies Used:</h4>
@@ -45,6 +47,7 @@ export default function Projects() {
                     <h2>Boat Better</h2>
                     <div className='button-holder'>
                     <a className="website" href='https://boat-better.netlify.app' target="_blank">VIEW</a>
+                    <a className="website" href='https://github.com/kalebmcc/Boat-Better' target="_blank">REPO</a>
                     </div>
                     <p>This application utilizes the Stormglass.io API to display weather information on the provided Latitude and Longitude. It can display the weather for 7 days, or for each hour on one of the 7 days.</p><br />
                     <h4>Technologies Used:</h4>
@@ -66,6 +69,7 @@ export default function Projects() {
                     <h2>Simon Game</h2>
                     <div className='button-holder'>
                     <a className="website" href='https://parkpassadventure.netlify.app/' target="_blank">VIEW</a>
+                    <a className="website" href='https://github.com/kalebmcc/Simon_Game' target="_blank">REPO</a>
                     </div>
                     <p>ParkPass is a full-stack application that allows a user to search through a database of national parks and save them. When saved, a park will be added to a user’s page and displayed as a card. Users can then check off if they’ve hiked at the selected park.</p><br />
                     <h4>Technologies Used:</h4>
